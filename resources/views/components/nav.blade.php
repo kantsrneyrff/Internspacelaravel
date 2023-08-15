92e7e2cec58235a8a46d46a4816f54b6882493a7
kantsrneyrff <davicastroo123@gmail.com>
2023-08-15 18:48:07 -0300

    .

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,7 +5,7 @@
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
     <form id="navbar-temporizador" class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-        <div style="color: green; font-weight: bold;" id="temporizador"></div>
+        <div style=" color:#AA48E5; font-weight: bold;" id="temporizador"></div>
     </form>
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
