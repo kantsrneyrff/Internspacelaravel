6056d637a5a21e285e66928e08d1075b2ebce122
raphael0627f <raphaelferreirafo@gmail.com>
2023-08-23 17:59:45 -0300

    .

--- resources/views/painel/perfil/index.blade.php
+++ resources/views/painel/perfil/index.blade.php
@@ -15,80 +15,57 @@
         <div class="d-flex flex-col justify-content-center text-center align-items-center mx-4">
             <div class="profile-details">
                 <h3 style="text-transform: uppercase;">
-                    <b>
-
-                    </b>
+                    <b></b>
                 </h3>
-                <h5>
-
-                </h5>
+                <h5></h5>
             </div>
         </div>
         <div class="d-flex flex-col justify-content-center text-center align-items-center my-3 mb-5">
-
             <button class="btn btn-outline-secondary mx-2" type="button" onclick="mostrarEditarDados(this)" id="btn-perfil-dados" style="border-radius: 100px">Editar dados</button>
             <button class="btn btn-outline-secondary mx-2" type="button" onclick="mostrarAlterarFoto(this)" id="btn-perfil-foto" style="border-radius: 100px">Alterar foto</button>
-
         </div>
         <div class="d-flex flex-col justify-content-start text-start align-items-center">
             <div>
                 <h5><b>CONTATO</b></h5>
                 <div class="profile-contact-email my-4">
-
                     <h6><b></b></h6>
                     <span>
-
                     </span>
-
                 </div>
-
                 <div class="profile-contact-phone my-4">
                     <h6><b>Telefone</b></h6>
                     <span>{{auth()->user()->telefone}}</span>
                 </div>
-
             </div>
         </div>
-
     </div>
 </div>
 <!-- Cartão 2 -->
 <div class="card p-4 m-2 flex-wrap text-nowrap flex-column flex-grow-1">
-
     <div class="d-flex flex-col align-items-center m-2">
         <div class="profile-details">
             <h5><b>DATA DE NASCIMENTO</b></h5>
             <h6>{{date('d/m/Y', strtotime(auth()->user()->dataNascimento))}}</h6>
         </div>
     </div>
-
-
     <div class="d-flex flex-col align-items-center m-2">
         <div class="profile-details">
             <h5><b>ESTADO</b></h5>
             <h6>{{auth()->user()->uf}}</h6>
         </div>
     </div>
-
-
     <div class="d-flex flex-col align-items-center m-2">
         <div class="profile-details">
             <h5><b>CIDADE</b></h5>
             <h6></h6>
         </div>
     </div>
-
-
     <div class="d-flex flex-col align-items-center m-2">
         <div class="profile-details">
             <h5><b>BAIRRO</b></h5>
             <h6></h6>
         </div>
     </div>
-
-
-</div>
-</div>
 </div>
 <!-- Modal -->
 <div class="modal fade" id="modal-dados" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -103,14 +80,13 @@
             <form action="" method="POST">
                 <div class="modal-body">
                     boua<br><br>
-
                     <input type="hidden" name="id" id="id">
                     <input type="hidden" name="acao" value="1">
-                    <strong> Nome: </strong><input type="text" id="nome" disabled=""><br>
-                    <strong>Data </strong><input type="text" id="data" disabled=""><br>
-                    <strong>Período: </strong><input type="text" id="periodo" disabled=""><br>
-                    <strong>Local: </strong><input type="text" id="local" disabled=""><br>
-                    <strong>Setor: </strong><input type="text" id="setor" disabled="">
+                    <strong>Nome:</strong><input type="text" id="nome" disabled=""><br>
+                    <strong>Data:</strong><input type="text" id="data" disabled=""><br>
+                    <strong>Período:</strong><input type="text" id="periodo" disabled=""><br>
+                    <strong>Local:</strong><input type="text" id="local" disabled=""><br>
+                    <strong>Setor:</strong><input type="text" id="setor" disabled="">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" onclick="$('#modal-dados').modal('hide')">Cancelar</button>
