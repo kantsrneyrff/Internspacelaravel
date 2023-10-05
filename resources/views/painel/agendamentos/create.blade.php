bd5213341725a7bfa613a62fc47d1801a48db90a
NavyBlue1 <navybluue.1@gmail.com>
2023-10-04 21:29:37 -0300

    .

--- resources/views/painel/agendamentos/create.blade.php
+++ resources/views/painel/agendamentos/create.blade.php
@@ -1,5 +1,6 @@
-<title>Solicitar Agendamento</title>
 @extends('layouts.template')
+
+@section('title','Solicitar agendamento')
 
 @section('head')
 <link rel="stylesheet" href="/css/agendamento.css">
@@ -17,7 +18,7 @@
                         <div class="container">
                             <div class="row">
                                 <div class="form-group col-md-12">
-                                    <h2 class="mt-lg-1 mt-3 mb-4"><b>Solicitar Agendamento</b></h2>
+                                    <h3 class="mt-lg-0 mt-3 mb-4"><b>Selecionar opções</b></h3>
                                     <label for="local">Local:</label>
                                     <select class="form-select @error('idLocal') is-invalid @enderror" id="local" onchange="limitarSetores()" name="idLocal">
                                         <option value="">Selecione</option>
