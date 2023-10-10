c2c055ad6d2ad7f3ed8b91b7699358f6da3a4a45
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2023-10-10 00:03:15 -0300

    .

--- resources/views/painel/painel.blade.php
+++ resources/views/painel/painel.blade.php
@@ -1,45 +1,42 @@
 @extends('layouts.template')
 
 @section('title')
-Olá, {{ auth()->user()->nome }}
+    Olá, {{ auth()->user()->nome }}
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-xl-6">
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-chart-bar me-1"></i>
-                Horas/Setor
-            </div>
-            <div class="card-body" style="position: relative;">
-                <canvas id="myBarChart" width="100%" height="40"></canvas>
-
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6">
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-chart-pie me-1"></i>
-                Agendamentos/Setor
-            </div>
-            <div class="card-body" style="position: relative;">
-                <canvas id="myPieChart" width="100%" height="40"></canvas>
-
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6">
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fa-solid fa-bars-progress"></i>
-                Progresso
-            </div>
-            <div class="p-4">
-                <div style="text-align: right;">
-                    <a class="r-20" style="text-decoration:none;"><b>230h</b></a>
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-chart-bar me-1"></i>
+                    Horas/Setor
                 </div>
+                <div class="card-body" style="position: relative;">
+                    <canvas id="myBarChart" width="100%" height="40"></canvas>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-chart-pie me-1"></i>
+                    Agendamentos/Setor
+                </div>
+                <div class="card-body" style="position: relative;">
+                    <canvas id="myPieChart" width="100%" height="40"></canvas>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fa-solid fa-bars-progress"></i>
+                    Progresso
+                </div>
+                <a class="r-20" style="text-align: right; margin-right: 10px; text-decoration:none;">230h</a>
                 <div class="progress">
                     <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"
                         style="width:55%">
@@ -53,53 +50,44 @@ Olá, {{ auth()->user()->nome }}
 
     </div>
 
+    </main>
 
-</div>
+    </div>
 
-</div>
-
-</main>
-
-</div>
-
-</div>
+    </div>
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
 
 
-<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#tabela').DataTable({
-            language: {
-                url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/pt-BR.json',
-            },
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#tabela').DataTable({
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.13.5/i18n/pt-BR.json',
+                },
+            });
         });
-    });
 
 
-    var ctx = document.getElementById('myPieChart');
+        var ctx = document.getElementById('myPieChart');
 
-    var setoresNomes = {
-        !!json_encode($setoresNomes) !!
-    };
-    var setoresValores = {
-        !!json_encode($valores) !!
-    };
+        var setoresNomes = {!! json_encode($setoresNomes) !!};
+        var setoresValores = {!! json_encode($valores) !!};
 
-    var data = {
-        labels: setoresNomes,
-        datasets: [{
-            data: setoresValores,
-            backgroundColor: ['#4F79E4', '#7DDD4F', '#ED2139', '#F8B534', '#aa48e5'],
-            borderWidth: 1
-        }]
-    };
+        var data = {
+            labels: setoresNomes,
+            datasets: [{
+                data: setoresValores,
+                backgroundColor: ['#4F79E4', '#7DDD4F', '#ED2139', '#F8B534', '#aa48e5'],
+                borderWidth: 1
+            }]
+        };
 
-    var options = {
-        responsive: true
-    };
+        var options = {
+            responsive: true
+        };
 
         var myPieChart = new Chart(ctx, {
             type: 'pie',
@@ -107,7 +95,7 @@ Olá, {{ auth()->user()->nome }}
             options: options
         });
 
-var setoresNomes = {!! json_encode($setoresNomes) !!};
+      var setoresNomes = {!! json_encode($setoresNomes) !!};
 var valores = {!! json_encode($valores) !!};
 var setorLabel = '{{ $setorLabel }}';
 var setorIds = {!! json_encode($setorIds) !!};
@@ -169,6 +157,5 @@ if (typeof setoresNomes !== 'undefined') {
 
 
     </script>
-@endsection
 @endsection
 @endsection
