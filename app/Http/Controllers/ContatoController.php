1f6fa556d743a5fb23ea575f5d68e00de49337fe
raphael0627f <raphaelferreirafo@gmail.com>
2023-10-19 17:28:07 -0300

    .

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -25,5 +25,6 @@ class ContatoController extends Controller
         $header .= "MIME-Version: 1.0" . "\r\n";
         $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
         mail($to, $subject, $body, $header);
+        return redirect()->route('home');
     }
 }
